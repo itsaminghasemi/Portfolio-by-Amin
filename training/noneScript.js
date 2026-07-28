fb97849cb239cc70f49bd204143b196bb501fb31
itsaminghasemi <itsaminghasemi@gmail.com>
2026-07-28 15:27:31 +0330

    update noneScript.js

--- training/noneScript.js
+++ training/noneScript.js
@@ -1,13 +1,3 @@
 "use strict";
 
-const fruit = {
-  red: {
-    swrawberry: { a: 1 },
-    apple: {},
-  },
-};
-
-const { red } = fruit;
-const { swrawberry } = red;
-
-console.log(swrawberry);
+const allDivs = event.target.closet("li");
