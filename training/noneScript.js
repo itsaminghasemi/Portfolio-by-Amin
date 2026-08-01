546ef302557cd7dffc419e768383813d5545baf5
itsaminghasemi <itsaminghasemi@gmail.com>
2026-08-01 09:59:41 +0330

    keep this

--- training/noneScript.js
+++ training/noneScript.js
@@ -1,3 +1,21 @@
 "use strict";
 
-const allDivs = event.target.closet("li");
+const str = "string";
+
+const array_2 = [...str, "", "is so good"];
+
+console.log(...array_2);
+
+const inngrediants = [
+  prompt(
+    "let's create a magic: just give me your choice as ... then click enter",
+  ),
+  prompt(
+    "let's create a magic: just give me your choice as ... then click enter",
+  ),
+  prompt(
+    "let's create a magic: just give me your choice as ... then click enter",
+  ),
+];
+
+console.log(inngrediants);
