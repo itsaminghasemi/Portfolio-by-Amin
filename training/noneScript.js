c328b87c9260cf992bbb17f98f9c0b9ff5a2c9b9
itsaminghasemi <itsaminghasemi@gmail.com>
2026-08-02 17:13:30 +0330

    keep this

--- training/noneScript.js
+++ training/noneScript.js
@@ -1,21 +1,3 @@
 "use strict";
 
-const str = "string";
-
-const array_2 = [...str, "", "is so good"];
-
-console.log(...array_2);
-
-const inngrediants = [
-  prompt(
-    "let's create a magic: just give me your choice as ... then click enter",
-  ),
-  prompt(
-    "let's create a magic: just give me your choice as ... then click enter",
-  ),
-  prompt(
-    "let's create a magic: just give me your choice as ... then click enter",
-  ),
-];
-
-console.log(inngrediants);
+Array.style.transform.scale = 1.1;
