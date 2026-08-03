f6ddac792c03fef34d9d31307da5dde84395f11b
itsaminghasemi <itsaminghasemi@gmail.com>
2026-08-03 17:36:52 +0330

    keep this

--- training/noneScript.js
+++ training/noneScript.js
@@ -1,3 +1,14 @@
 "use strict";
 
-Array.style.transform.scale = 1.1;
+const user = {
+  name: "Amin",
+  "is-dev": true, // Quotes required for hyphens/spaces
+  100: "score", // Number key becomes string "100"
+};
+
+const newUser = structuredClone(user);
+Object.assign(newUser, { name: "Ishagh", "is-dev": false, 100: "finalScope" });
+
+// Dot Notation (Static, known keys)
+console.log(user.name);
+console.log(`${newUser.name} - ${newUser["is-dev"]} - ${newUser[100]} `);
