529c7d99aa1b97d1da638035c179aa28b24dde7b
itsaminghasemi <itsaminghasemi@gmail.com>
2026-08-09 14:55:15 +0330

    work on contact fileUploader

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,7 @@
 // Dynamic footer year
-document.addEventListener('DOMContentLoaded', () => {
-  const yearEl = document.getElementById('year');
+document.addEventListener("DOMContentLoaded", () => {
+  const yearEl = document.getElementById("year");
   if (yearEl) yearEl.textContent = new Date().getFullYear();
 });
+
+const fileUploader = document.querySelector(".file-upload-label");
