3ca1b6880f6937034657adf5dcb9f6e7f2a5f552
Amin Ghasemi <itsaminghasemi@gmail.com>
2026-08-22 08:38:30 +0330

    when you are press the menu, guide was hid

--- js/index-logic.js
+++ js/index-logic.js
@@ -1,5 +1,19 @@
 // Dynamic footer year
-document.addEventListener('DOMContentLoaded', () => {
-  const yearEl = document.getElementById('year');
-  if (yearEl) yearEl.textContent = new Date().getFullYear();
-});
+
+"use strict";
+
+(function () {
+  document.addEventListener("DOMContentLoaded", () => {
+    const yearEl = document.getElementById("year");
+    if (yearEl) yearEl.textContent = new Date().getFullYear();
+  });
+
+  const menuBtn = document.getElementById("nav-toggle");
+  const guideBtn = document.querySelector(".guide-btn");
+
+  menuBtn.addEventListener("click", () => {
+    guideBtn.array.forEach((el) => {
+      el.style.display = "none";
+    });
+  });
+})();
