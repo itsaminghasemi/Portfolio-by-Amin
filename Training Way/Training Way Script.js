347f4ba6ea40a22299bbaf564ddd80c79778ef92
Amin Ghasemi <itsaminghasemi@gmail.com>
2026-09-06 07:08:34 +0330

    feat: Implement greeting functions and enhance main layout for better content display

--- Training Way/Training Way Script.js
+++ Training Way/Training Way Script.js
@@ -1,2 +1,20 @@
 const message = 'Hello from the console!';
 console.log(message);
+
+function createGreeting(name) {
+  const greeting = `Hello, ${name}! Welcome`;
+
+  return function () {
+    return greeting;
+  };
+}
+
+const greetAmin = createGreeting('Amin');
+
+console.log(greetAmin());
+// "Hello, Amin! Welcome"
+
+const greetAli = createGreeting('Ali');
+
+console.log(greetAli());
+// "Hello, Ali! Welcome"
